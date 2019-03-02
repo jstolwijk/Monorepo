8107507853a54a57856ae020ebc53eb1467353b9
Jesse Stolwijk <stolwijkjesse@gmail.com>
2019-03-02 16:15:49 +0100

    blinking led

--- raspberry/src/main/java/org/jessestolwijk/raspberry/Main.kt
+++ raspberry/src/main/java/org/jessestolwijk/raspberry/Main.kt
@@ -1,5 +1,22 @@
 package org.jessestolwijk.raspberry
 
-fun main() {
-    println("Hello World!")
+import com.pi4j.io.gpio.GpioFactory
+import com.pi4j.io.gpio.PinState
+import com.pi4j.io.gpio.RaspiPin
+import sun.nio.cs.Surrogate.low
+
+fun main(args: Array<String>) {
+    val gpio = GpioFactory.getInstance()
+
+    val pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "PinLED", PinState.HIGH)
+    println("light is: ON")
+
+    Thread.sleep(2000)
+
+    pin.low()
+    println("light is: OFF")
+    Thread.sleep(1000)
+    println("light is: ON for 1 second")
+    pin.pulse(1000, true)
+    gpio.shutdown()
 }
