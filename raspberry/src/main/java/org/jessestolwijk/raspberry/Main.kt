f7b9491de3295ffe37206c3df99fc13b4a901174
Jesse Stolwijk <stolwijkjesse@gmail.com>
2019-03-09 22:42:50 +0100

    add elecontron typescript sample

--- raspberry/src/main/java/org/jessestolwijk/raspberry/Main.kt
+++ raspberry/src/main/java/org/jessestolwijk/raspberry/Main.kt
@@ -1,22 +1,38 @@
 package org.jessestolwijk.raspberry
 
+import com.pi4j.io.gpio.GpioController
 import com.pi4j.io.gpio.GpioFactory
-import com.pi4j.io.gpio.PinState
 import com.pi4j.io.gpio.RaspiPin
-import sun.nio.cs.Surrogate.low
 
-fun main(args: Array<String>) {
-    val gpio = GpioFactory.getInstance()
+fun main() {
+    println("Hello World")
+    runtime.onShutdown {
+        if(!gpio.isShutdown) {
+            gpio.shutdown()
+        }
+        println("shutdown")
+    }
 
-    val pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "PinLED", PinState.HIGH)
-    println("light is: ON")
+    gpioController {
 
-    Thread.sleep(2000)
+        provisionDigitalOutputPin(RaspiPin.GPIO_00, "PinLED").also { pin ->
+            repeat(20) {
+                pin.low()
+                Thread.sleep(100)
+                pin.high()
+                Thread.sleep(100)
+            }
+        }
 
-    pin.low()
-    println("light is: OFF")
-    Thread.sleep(1000)
-    println("light is: ON for 1 second")
-    pin.pulse(1000, true)
-    gpio.shutdown()
+        shutdown()
+    }
+
+
+    println("Finished")
 }
+
+private val gpio: GpioController = GpioFactory.getInstance()
+fun gpioController(block: GpioController.() -> Unit) = gpio.block()
+
+private val runtime = Runtime.getRuntime()
+private fun Runtime.onShutdown(block: () -> Unit) = addShutdownHook(Thread { block() })
